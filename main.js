1887927515bfd51f9ab7afa1d4b52a92b4a61bf1
imshubhamSaha <imshubhamsaha@gmail.com>
2023-02-20 23:38:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
+
+myForm.addEventListener("submit", onSubmit);
+let user = 1;
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (nameInput.value === "" || !emailInput.value.includes("@")) {
+    msg.classList.add("error");
+    msg.innerHTML = "Please enter all fields";
+
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement("li");
+
+    li.appendChild(
+      document.createTextNode(`${nameInput.value}: ${emailInput.value}`)
+    );
+    const userData = `${nameInput.value}:${emailInput.value}`;
+    // Append to ul
+    userList.appendChild(li);
+    localStorage.setItem(`User${user}`, userData);
+    user++;
+    nameInput.value = "";
+    emailInput.value = "";
+  }
+}
