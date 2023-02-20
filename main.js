e4e81bdae182d84ed4d949e1f59b0080f3dcec60
imshubhamSaha <imshubhamsaha@gmail.com>
2023-02-21 01:53:12 +0530

    add removing data from storage in realtime feature

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const msg = document.querySelector(".msg");
 const userList = document.querySelector("#users");
 
 myForm.addEventListener("submit", onSubmit);
+userList.addEventListener("click", removeItem);
 
 function onSubmit(e) {
   e.preventDefault();
@@ -16,20 +17,34 @@ function onSubmit(e) {
     setTimeout(() => msg.remove(), 3000);
   } else {
     const li = document.createElement("li");
+    const btn = document.createElement("button");
 
-    li.appendChild(
-      document.createTextNode(`${nameInput.value}: ${emailInput.value}`)
-    );
+    li.appendChild(document.createTextNode(`${nameInput.value} :`));
+    li.appendChild(document.createTextNode(` ${emailInput.value}`));
+
+    btn.appendChild(document.createTextNode("Delete"));
+    btn.classList.add("delBtn");
 
     const userData = JSON.stringify({
       name: nameInput.value,
       email: emailInput.value,
     });
-
+    li.appendChild(btn);
     // Append to ul
     userList.appendChild(li);
     localStorage.setItem(`${emailInput.value}`, userData);
     nameInput.value = "";
     emailInput.value = "";
+  }
+}
+
+function removeItem(e) {
+  if (e.target.classList.contains("delBtn")) {
+    if (confirm("Are You Sure?")) {
+      const key = e.target.parentElement.childNodes[1].nodeValue.trim();
+      const li = e.target.parentElement;
+      userList.removeChild(li);
+      localStorage.removeItem(`${key}`);
+    }
   }
 }
